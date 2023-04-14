f8f065060fe7c497d5ec8e76a9b8007184951be9
Slawomir.Ploszaj <slawomirploszaj.gmail.com>
2023-04-14 14:20:55 +0200

    button number corrected

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -8,6 +8,21 @@ public class Kalkulator {
     private JButton button3;
     private JButton button4;
     private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+    private JButton button17;
+    private JButton button18;
+    private JButton button19;
+    private JButton button20;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Kalkulator");
